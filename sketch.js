097719c3fdb1051145b5e46d2442c3684fa5b53f
Joaovictorquiezi <joao.quiezi@escola.pr.gov.br>
2024-09-11 22:40:12 +0000

    jvq

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+function setup() {
+      createCanvas(400, 400);
+        background("#673AB7");
+        }
+
+        function draw() {
+          stroke("#FFEB3B");
+            fill("#8BC34A");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 20);
+                          }
+                          }
 
